0e05c58ee8821df948fae23a3b00372cea87ad03
jaredspillari <jaredspillari@gmail.com>
2025-10-16 17:49:52 -0600

    week06

---
+++ project/js/main.js
@@ -0,0 +1,19 @@
+// Reveal on scroll
+const reveals = document.querySelectorAll('.reveal');
+
+function reveal() {
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const revealTop = reveals[i].getBoundingClientRect().top;
+        const revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+window.addEventListener('scroll', reveal);
+reveal(); // Inicial
